2f6f7eceaaafb47a90339dca57b81c499eda5441
MinhPC <42643748+MinhPCSE04098@users.noreply.github.com>
2019-10-06 18:21:20 +0700

    Add files via upload

---
+++ Design/Source/IDCS/src/java/entities/UserBean.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author macbook
+ */
+public class UserBean {
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String token;
+
+    public UserBean() {
+    }
+    
+    public UserBean(UserBean ub){
+        
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
+   
+}
