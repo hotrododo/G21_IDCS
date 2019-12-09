ef8e4e8ea229e38f44c568abc8fde78c86fec233
DonNQ <>
2019-12-09 23:41:09 +0700

    update filter

---
+++ Source/DonNQ/IDCS/src/main/java/CSTCopyright/IDCS/filter/CookieFilter.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CSTCopyright.IDCS.filter;
+
+import CSTCopyright.IDCS.controller.UserAccount;
+import CSTCopyright.IDCS.utils.DBUtils;
+import CSTCopyright.IDCS.utils.MyUtils;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author mac
+ */
+
+@WebFilter(filterName = "cookieFilter", urlPatterns = { "/*" })
+public class CookieFilter implements Filter{
+
+    public CookieFilter() {
+    }
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpSession session = req.getSession();
+ 
+        UserAccount userInSession = MyUtils.getLoginedUser(session);
+        // 
+        if (userInSession != null) {
+            session.setAttribute("COOKIE_CHECKED", "CHECKED");
+            chain.doFilter(request, response);
+            return;
+        }
+ 
+        // Connection was created in JDBCFilter.
+        Connection conn = MyUtils.getStoredConnection(request);
+ 
+        // Flag check cookie
+        String checked = (String) session.getAttribute("COOKIE_CHECKED");
+        if (checked == null && conn != null) {
+            String userName = MyUtils.getUserNameInCookie(req);
+            try {
+                UserAccount user = DBUtils.findUser(conn, userName);
+                MyUtils.storeLoginedUser(session, user);
+            } catch (SQLException ex) {
+                Logger.getLogger(CookieFilter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            // Mark checked Cookies.
+            session.setAttribute("COOKIE_CHECKED", "CHECKED");
+        }
+ 
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
