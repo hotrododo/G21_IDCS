dbf537ac36da915ccbd80d089db9d73384a3d087
DonNQ <>
2019-12-14 00:01:59 +0700

    update python community

--- Source/DonNQ/IDCS/src/main/java/CSTCopyright/IDCS/services/ScanServices.java
+++ Source/DonNQ/IDCS/src/main/java/CSTCopyright/IDCS/services/ScanServices.java
@@ -18,27 +18,29 @@ import java.net.UnknownHostException;
  * @author mac
  */
 public class ScanServices {
+
     private final String serverHost = "localhost";
     private final int serverPort = 12017;
     private Socket socketOfClient = null;
     private BufferedWriter os = null;
     private BufferedReader is = null;
-    
+
     // Khởi tạo kết nối tới server
     public boolean initConn() throws IOException {
         try {
-            socketOfClient = new Socket(serverHost, 9999);
+            socketOfClient = new Socket(serverHost, serverPort);
+            socketOfClient.setReceiveBufferSize(4096);
         } catch (final UnknownHostException e) {
             System.err.println("Don't know about host " + serverHost);
             return false;
         }
         return true;
     }
-    
+
     // gửi data tới server và nhận lại dữ liệu.
     // Nếu return không phải string data thì kết nối failed
-    public String dataTransfer(final String str) throws IOException {
-        String mess = null;
+    public String dataTransfer(final String str) {
+        StringBuilder mess = null;
         try {
             // Tạo luồng đầu ra tại client (Gửi dữ liệu tới server)
             os = new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream()));
@@ -47,27 +49,32 @@ public class ScanServices {
             is = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
 
             os.write(str);
-            while (is.readLine() != null) {
-                mess += is.readLine();
+            os.newLine();
+            os.flush();
+            String responseLine = "";
+            while (responseLine.equals("[END-PCK]")) {
+                responseLine = is.readLine();
+                System.out.println("Server:" + responseLine);
+                if(responseLine != null) mess.append(responseLine);
             }
+            if(os != null) os.close();
+            if(is != null) is.close();
         } catch (final IOException e) {
             System.err.println("Couldn't get I/O for the connection to " + serverHost);
             return "-1";
-        }
-        os.close();
-        is.close();
-        return mess;
+        } 
+        return mess.toString();
     }
-    
+
     //Đóng kết nối tới server
     //return true: đóng được kết nối
     //return false: không thể đóng kết nối.
-    public boolean closeConn() throws IOException{
-        if(socketOfClient != null){
+    public boolean closeConn() throws IOException {
+        if (socketOfClient != null) {
             os = new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream()));
             is = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
             os.write("Quit");
-            if(is.readLine().equals("OK")){
+            if (is.readLine().equals("OK")) {
                 socketOfClient.close();
                 socketOfClient = null;
                 return true;
