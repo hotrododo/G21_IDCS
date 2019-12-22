e747da4971f456bcdebfc27382cb27b13a2365f8
DonNQ <>
2019-12-22 21:38:13 +0700

    update lib

---
+++ Source/DonNQ/IDCS/src/main/java/CSTCopyright/IDCS/servlet/UserUpdateServlet.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CSTCopyright.IDCS.servlet;
+
+import CSTCopyright.IDCS.controller.UserAccount;
+import CSTCopyright.IDCS.utils.DBUtils;
+import CSTCopyright.IDCS.utils.MyUtils;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author mac
+ */
+public class UserUpdateServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        // Check User has logged on
+        UserAccount loginedUser = MyUtils.getLoginedUser(session);
+
+        // Not logged in
+        if (loginedUser == null) {
+            // Redirect to login page.
+            response.sendRedirect(request.getContextPath() + "/login");
+            return;
+        }
+        String email = (String)request.getParameter("txt1");
+        String fullName = (String)request.getParameter("txt2");
+        String phone = (String)request.getParameter("txt3");
+        String address = (String)request.getParameter("txt4");
+        UserAccount user = loginedUser;
+        user.setEmailAddress(email);
+        user.setFullName(fullName);
+        user.setPhoneNumber(phone);
+        user.setAddress(address);
+        Connection conn = MyUtils.getStoredConnection(request);
+        boolean isUpdated = DBUtils.updateUser(conn, user);
+        String mess = "";
+        if(isUpdated){
+            mess = "Update Successful";
+        }else{
+            mess = "Update Failed!";
+        }
+        request.setAttribute("mess", mess);
+        request.setAttribute("user", user);
+        request.setAttribute("uds", "1"); //set user-display-state = 1
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/views/userInfoView.jsp");
+        dispatcher.forward(request, response);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
